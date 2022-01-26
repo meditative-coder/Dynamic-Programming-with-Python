86d618f8f53dac5e249a8618544652dc22088aad
ankit <ankitpysde@gmail.com>
2022-01-26 13:22:41 +0530

    LCS

--- longest_common_subsequence.py
+++ longest_common_subsequence.py
@@ -5,25 +5,55 @@ y: abedfhr
 find longest common subsequence, here it is len(abdh) i.e. 4
 '''
 
-def lcs_recursive(x,y,n,m):
+def lcs_recursive(x,y,m,n):
     # to write base condition, think of the smallest valid input
     if n==0 or m==0:
         return 0
-    if x[n-1]==y[m-1]:
-        return 1 + lcs_recursive(x,y,n-1,m-1)
+    if x[m-1]==y[n-1]:
+        return 1 + lcs_recursive(x,y,m-1,n-1)
     else:
         return max(
-            lcs_recursive(x, y, n-1, m),
-            lcs_recursive(x, y, n, m-1)
+            lcs_recursive(x, y, m-1, n),
+            lcs_recursive(x, y, m, n-1)
         )
 
-def lcs_memoize():
-    pass
+t = [[-1 for j in range(101)] for i in range(101)]
+def lcs_memoize(x,y,m,n):
+    global t
+    if m==0 or n==0:
+        return 0
+    if t[m][n] != -1:
+        return t[m][n]
+    if x[m-1] == y[n-1]:
+        t[m][n] = 1 + lcs_memoize(x,y,m-1,n-1)
+        return t[m][n]
+    else:
+        t[m][n]= max(
+            lcs_memoize(x,y,m-1,n),
+            lcs_memoize(x,y,m,n-1)
+        )
+        return t[m][n]
 
-def lcs_tabulate():
-    pass
+def lcs_tabulate(x,y,m,n):
+    t = [[-1 for j in range(n+1)] for i in range(m+1)]
+    for i in range(m+1):
+        for j in range(0,n+1):
+            if i==0 or j==0:
+                t[i][j] = 0
+            elif x[i-1] == y[j-1]:
+                t[i][j] = 1 + t[i-1][j-1]
+            else:
+                t[i][j] = max(
+                    t[i-1][j],
+                    t[i][j-1]
+                )
+    return t[m][n]
+
+    
 
 if __name__ == "__main__":
     x =  "abcdgh"
     y =  "abedfhr"
     print("Recursive", lcs_recursive(x,y,len(x),len(y)))
+    print("Memoize", lcs_memoize(x,y,len(x),len(y)))
+    print("Tabulation", lcs_tabulate(x,y,len(x),len(y)))
---
+++ longest_common_substring.py
@@ -0,0 +1,29 @@
+'''                                     VARIANT OF LCS                                       '''
+from threading import currentThread
+
+
+def lcs_tabulate(x,y,m,n):
+    t = [[-1 for j in range(n+1)] for i in range(m+1)]
+    for i in range(m+1):
+        for j in range(0,n+1):
+            if i==0 or j==0:
+                t[i][j] = 0
+            elif x[i-1] == y[j-1]:
+                t[i][j] = 1 + t[i-1][j-1]
+            # this else condition is only different with longest common subsequence
+            ## Instead of selecting max, on discontinuity start from 0
+            else:
+                t[i][j] = 0
+    current_max = 0
+    for i in range(m+1):
+        for j in range(n+1):
+            if t[i][j] > current_max:
+                current_max = t[i][j]
+    return current_max
+
+    
+
+if __name__ == "__main__":
+    x =  "abcdef"
+    y =  "abchfg"
+    print("Tabulation", lcs_tabulate(x,y,len(x),len(y)))
